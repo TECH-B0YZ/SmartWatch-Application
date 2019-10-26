db9f443e65a254280e74be3e49c81635992c3b4a
kwitaszek <kevin.witaszek@gmail.com>
2019-10-25 20:35:18 -0400

    Added Functionality(Kevin)

--- SmartWatch/src/main/java/smart/watch/HomeFragment.java
+++ SmartWatch/src/main/java/smart/watch/HomeFragment.java
@@ -9,13 +9,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment  extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        TextView tv = root.findViewById(R.id.greeting);
+        tv.setText(getString(R.string.hello_home));
+
+
+        return root;
     }
 }
