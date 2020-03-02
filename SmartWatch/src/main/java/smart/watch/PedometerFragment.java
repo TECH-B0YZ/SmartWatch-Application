4930f578ec2900f39581f5efadef9310fee5fd98
kwitaszek <kevin.witaszek@gmail.com>
2020-03-02 13:45:54 -0500

    Added first implementation for bluetooth. Kevin

--- SmartWatch/src/main/java/smart/watch/PedometerFragment.java
+++ SmartWatch/src/main/java/smart/watch/PedometerFragment.java
@@ -6,7 +6,11 @@
 package smart.watch;
 
 import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.PointF;
@@ -38,17 +42,16 @@ import com.hookedonplay.decoviewlib.DecoView;
 import com.hookedonplay.decoviewlib.charts.EdgeDetail;
 import com.hookedonplay.decoviewlib.charts.SeriesItem;
 import com.hookedonplay.decoviewlib.events.DecoEvent;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.ValueDependentColor;
-import com.jjoe64.graphview.series.BarGraphSeries;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
-import com.jjoe64.graphview.series.PointsGraphSeries;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -61,24 +64,36 @@ public class PedometerFragment extends Fragment implements SensorEventListener {
     private String email_sharedPrefs;
     private TextView tv_steps, tv_goal;
     private EditText et;
-    private SensorManager sensorManager;
     private DecoView mDecoView;
     private Handler mHandler = new Handler();
     private boolean running = false;
     private float goal = 500;
+    private String stepValue;
 
     private int mSeries1Index;
     private float newPosition;
     private Runnable mTimer1;
-    private LineGraphSeries<DataPoint> mSeries1;
-
-
-    private double mLastRandom = 2;
-    private Random mRand = new Random();
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference SensorData = db.collection("Login Data");
     private Map<String, Object> note = new HashMap<>();
+
+
+    private final String DEVICE_ADDRESS="A4:CF:12:9A:CB:CE";
+    private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");//Serial Port Service ID
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    Button startButton, sendButton,clearButton,stopButton;
+    TextView textView;
+    EditText editText;
+    boolean deviceConnected=false;
+    //Thread thread;
+    byte buffer[];
+    //int bufferPosition;
+    boolean stopThread;
+
 
     private Runnable runnable = new Runnable() {
         @Override
@@ -96,12 +111,55 @@ public class PedometerFragment extends Fragment implements SensorEventListener {
 
 
         tv_steps = root.findViewById(R.id.steps);
-        sensorManager = (SensorManager)
-                Objects.requireNonNull(getActivity()).getSystemService(Context.SENSOR_SERVICE);
         mDecoView = root.findViewById(R.id.dynamicArcView);
         createDataSeries1();
         // Start the timer
         mHandler.post(runnable);
+
+        startButton = (Button) root.findViewById(R.id.buttonStart);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(BTinit())
+                {
+                    if(BTconnect())
+                    {
+                        setUiEnabled(true);
+                        deviceConnected=true;
+                        beginListenForData();
+                        Toast.makeText(getActivity(),"ESP32 connected,",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
+
+        stopButton = (Button) root.findViewById(R.id.buttonStop);
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopThread = true;
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //setUiEnabled(false);
+                deviceConnected=false;
+                Toast.makeText(getActivity(),"ESP32 disconnected",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         Button btn1 = root.findViewById(R.id.goal_btn);
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -118,65 +176,13 @@ public class PedometerFragment extends Fragment implements SensorEventListener {
                 }
             }
         });
-
-
-        GraphView graph = (GraphView) root.findViewById(R.id.graph);
-        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, -1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graph.addSeries(series);
-
-        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
-            @Override
-            public int get(DataPoint data) {
-                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
-            }
-        });
-
-        series.setSpacing(50);
-        graph.getGridLabelRenderer().setGridColor(Color.BLACK);
-        graph.getGridLabelRenderer().setVerticalLabelsColor(Color.BLACK);
-        graph.getGridLabelRenderer().setHorizontalLabelsColor(Color.BLACK);
-        graph.getGridLabelRenderer().setVerticalLabelsColor(Color.BLACK);
-        graph.getGridLabelRenderer().setHorizontalLabelsColor(Color.BLACK);
-        graph.getGridLabelRenderer().setVerticalLabelsColor(Color.BLACK);
-        graph.getGridLabelRenderer().setHorizontalLabelsColor(Color.BLACK);
-        graph.getGridLabelRenderer().setVerticalLabelsColor(Color.BLACK);
-        graph.getGridLabelRenderer().setHorizontalLabelsColor(Color.BLACK);
-        graph.getGridLabelRenderer().reloadStyles();
-
-        graph.getViewport().setScalable(true);
-        graph.getViewport().setScalableY(true);
-
         return root;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (running) {
-            loadEmail();
-            newPosition = event.values[0];
-            final String stepSend = String.valueOf(newPosition);
-            final String stepsTrimmed = stepSend.substring(0, stepSend.length() - 2);
-            tv_steps.setText(stepsTrimmed);
 
-            SensorData.document(loadEmail()).update(note).
-                    addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            note.put(KEY_STEPS, stepsTrimmed);
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(getContext(), getString(R.string.no_connection), Toast.LENGTH_SHORT).show();
-                        }
-                    });
         }
     }
 
@@ -184,15 +190,6 @@ public class PedometerFragment extends Fragment implements SensorEventListener {
     public void onResume() {
         super.onResume();
         running = true;
-        Sensor countSensor =
-                sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        if (countSensor != null) {
-            sensorManager.registerListener(this, countSensor,
-                    SensorManager.SENSOR_DELAY_UI);
-        } else {
-            Toast.makeText(getContext(), getString(R.string.sensor_not_found),
-                    Toast.LENGTH_SHORT).show();
-        }
 
         mTimer1 = new Runnable() {
             @Override
@@ -240,28 +237,145 @@ public class PedometerFragment extends Fragment implements SensorEventListener {
                 DecoEvent.Builder(newPosition).setIndex(mSeries1Index).build());
     }
 
-    private DataPoint[] generateData() {
-        int count = 30;
-        DataPoint[] values = new DataPoint[count];
-        for (int i = 0; i < count; i++) {
-            double x = i;
-            double f = mRand.nextDouble() * 0.15 + 0.3;
-            double y = Math.sin(i * f + 2) + mRand.nextDouble() * 0.3;
-
-            //double y = newPosition;
-            DataPoint v = new DataPoint(x, y);
-            values[i] = v;
-        }
-        return values;
-    }
-
     public String loadEmail() {
         SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         email_sharedPrefs = sharedPreferences.getString(EMAIL, "");
         return email_sharedPrefs;
     }
 
-    private double getRandom() {
-        return mLastRandom += mRand.nextDouble() * 0.5 - 0.25;
+
+    public void setUiEnabled(boolean bool)
+    {
+        //startButton.setEnabled(!bool);
+        //sendButton.setEnabled(bool);
+        //stopButton.setEnabled(bool);
+        //textView.setEnabled(bool);
+
+    }
+
+    public boolean BTinit()
+    {
+        boolean found=false;
+        BluetoothAdapter bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            //Toast.makeText(,"Device doesnt Support Bluetooth",Toast.LENGTH_SHORT).show();
+        }
+        if(!bluetoothAdapter.isEnabled())
+        {
+            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableAdapter, 0);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+        if(bondedDevices.isEmpty())
+        {
+            //Toast.makeText(getApplicationContext(),"Please Pair the Device first",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            for (BluetoothDevice iterator : bondedDevices)
+            {
+                if(iterator.getAddress().equals(DEVICE_ADDRESS))
+                {
+                    device=iterator;
+                    found=true;
+                    break;
+                }
+            }
+        }
+        return found;
+    }
+
+    public boolean BTconnect()
+    {
+        boolean connected=true;
+        try {
+            socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
+            socket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            connected=false;
+        }
+        if(connected)
+        {
+            try {
+                outputStream=socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                inputStream=socket.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        return connected;
+    }
+
+    void beginListenForData()
+    {
+        final Handler handler = new Handler();
+        stopThread = false;
+        buffer = new byte[1024];
+        Thread thread  = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopThread)
+                {
+                    try
+                    {
+                        int byteCount = inputStream.available();
+                        if(byteCount > 0)
+                        {
+                            byte[] rawBytes = new byte[byteCount];
+                            inputStream.read(rawBytes);
+                            final String string=new String(rawBytes);
+                            handler.post(new Runnable() {
+                                public void run()
+                                {
+                                    tv_steps.append(string);
+                                    stepValue = string;
+
+                                    loadEmail();
+                                    //newPosition = Float.valueOf(stepValue);
+                                    final String stepSend = String.valueOf(stepValue);
+                                    final String stepsTrimmed = stepSend.substring(0, stepSend.length() - 2);
+                                    tv_steps.setText(stepsTrimmed);
+
+                                    SensorData.document(loadEmail()).update(note).
+                                            addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    note.put(KEY_STEPS, stepsTrimmed);
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(getContext(), getString(R.string.no_connection), Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                }
+                            });
+
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopThread = true;
+                    }
+                }
+            }
+        });
+
+        thread.start();
     }
 }
